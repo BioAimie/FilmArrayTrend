1f9eaae57cb6b595b3b31d16fea1d51f1ee83fcb
Bionilesh <nilesh.ingle@biofiredx.com>
2016-12-19 08:00:38 -0700

    20161129_NormalizedMetricforQuantificationonDisease
    - updated main.r to extract Cp for customer pouch lot number from
    internal Cp data
    - updated main.r to plot comparison between customer Cp and internal Cp
    for PCR1, PCR2, YeastRNA

--- Nilesh/20161129_NormalizedMetricforQuantificationonDisease/main.r
+++ Nilesh/20161129_NormalizedMetricforQuantificationonDisease/main.r
@@ -1,6 +1,6 @@
 
 # set the path and load libraries and data
-workDir <-'C:/Users/nilesh_ingle/Documents/FilmArrayTrend/Nilesh/20161129_NormalizedMetricforQuantificationofDisease'
+workDir <-'C:/Users/nilesh_ingle/Documents/FilmArrayTrend/Nilesh/20161129_NormalizedMetricforQuantificationonDisease'
 sql_path <- 'C:/Users/nilesh_ingle/Documents/FilmArrayTrend/Nilesh/DataSources/SQL/20161129_NormalizedMetricforQuantificationonDisease'
 setwd(workDir)
 
@@ -37,14 +37,10 @@ for(i in 1:nrow(pouch.lot.number.df)){
   PMScxn <- odbcConnect('PMS_PROD')
   queryVector <- scan(paste(sql_path,'/FilmArray2_query-4.txt', sep=""),what=character(),quote="")
   query <- paste(queryVector,collapse=" ")
-  query <- gsub("([PouchLotNumber] LIKE \'%[0-9].*%\')", paste("[PouchLotNumber] LIKE ", "\'", "%", pouch.lot.number.df$PouchLotNumber[i],  "%", "\'", sep= ""), query)
-  cp.df.name <- paste("cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], sep="")
-  assign(cp.df.name,sqlQuery(PMScxn,query))
+  query <- gsub("\\[PouchLotNumber\\] LIKE \'%[0-9].*%\'", paste("[PouchLotNumber] LIKE ", "\'", "%", pouch.lot.number.df$PouchLotNumber[i],  "%", "\'", sep= ""), query)
+  internal.cp.df.name <- paste("internal.cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], sep="")
+  assign(internal.cp.df.name,sqlQuery(PMScxn,query))
   odbcClose(PMScxn)
-  #if(i%%3 == 0){Sys.sleep(3)} # momentarily stop the loop for '3 seconds' after '3' pouch lot numbers are scanned
-  
- # assign(cat(cp.df.name, "$PouchSerialNumber", sep=""), convert_to_factor(cat(cp.df.name, "$PouchSerialNumber", sep = "")))
- # assign(cat("cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], '$PouchLotNumber', sep = ""), convert_to_factor(cat("cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], '$PouchLotNumber', sep = "")))
 }
 
 # ===============================================================================================================
@@ -75,42 +71,42 @@ odbcClose(FADWcxn)
 
 # INTERNAL Cp data ---------------------------------------------------------------------------------------------------------------
 # convert pouch lot and serial numbers to factors for plotting
-cp.pouch.lot.number.df.332916$PouchSerialNumber <- as.factor(cp.pouch.lot.number.df.332916$PouchSerialNumber)
-cp.pouch.lot.number.df.332916$PouchLotNumber <- as.factor(cp.pouch.lot.number.df.332916$PouchLotNumber)
+internal.cp.pouch.lot.number.df.332916$PouchSerialNumber <- as.factor(internal.cp.pouch.lot.number.df.332916$PouchSerialNumber)
+internal.cp.pouch.lot.number.df.332916$PouchLotNumber <- as.factor(internal.cp.pouch.lot.number.df.332916$PouchLotNumber)
 
 # calculate mean and standard deviation 
-cp.pouch.lot.number.df.332916.avg <- aggregate(Cp ~ Name, cp.pouch.lot.number.df.332916, mean)
-summary.cp.pouch.lot.number.df.332916 <- summarySE(cp.pouch.lot.number.df.332916, measurevar="Cp", groupvars=c("Name"), conf.interval = 0.95, na.rm=TRUE)
+internal.cp.pouch.lot.number.df.332916.avg <- aggregate(Cp ~ Name, internal.cp.pouch.lot.number.df.332916, mean)
+summary.internal.cp.pouch.lot.number.df.332916 <- summarySE(internal.cp.pouch.lot.number.df.332916, measurevar="Cp", groupvars=c("Name"), conf.interval = 0.95, na.rm=TRUE)
 
 # plot the Cp values for internal controls
 dev.new()
 g1 <- ggplot() + ylab(cat("Cp", "\u00B1", "std.dev.")) + ggtitle("Internal Control Cp data for [PouchLotNumber] 332916")
-g1 <- g1 + geom_point(data = cp.pouch.lot.number.df.332916, aes(x = Name, y = Cp, fill = PouchSerialNumber), size = 1, alpha = 0.25)
-g1 <- g1 + geom_point(data = cp.pouch.lot.number.df.332916.avg, aes(x = Name, y = Cp), color = 'red', size = 2)
-g1 <- g1 + geom_errorbar(data = summary.cp.pouch.lot.number.df.332916, aes(x = Name, ymin = Cp - sd, ymax = Cp + sd))
+g1 <- g1 + geom_point(data = internal.cp.pouch.lot.number.df.332916, aes(x = Name, y = Cp, fill = PouchSerialNumber), size = 1, alpha = 0.25)
+g1 <- g1 + geom_point(data = internal.cp.pouch.lot.number.df.332916.avg, aes(x = Name, y = Cp), color = 'red', size = 2)
+g1 <- g1 + geom_errorbar(data = summary.internal.cp.pouch.lot.number.df.332916, aes(x = Name, ymin = Cp - sd, ymax = Cp + sd))
 g1
 
 # plot Cp histogram
 dev.new()
 gh1 <- ggplot() 
-gh1 <- gh1 + geom_histogram(data = cp.pouch.lot.number.df.332916, aes(Cp)) + facet_wrap(~Name)
+gh1 <- gh1 + geom_histogram(data = internal.cp.pouch.lot.number.df.332916, aes(Cp)) + facet_wrap(~Name)
 gh1
 
 # CUSTOMER Cp data ---------------------------------------------------------------------------------------------------------------
 # convert pouch lot and serial numbers to factors for plotting
-cust.cp.runs.df$PouchLotNumber <- as.factor(cp.runs.df$PouchLotNumber)
-cust.cp.runs.df$PouchSerialNumber <- as.factor(cp.runs.df$PouchSerialNumber)
+cust.cp.runs.df$PouchLotNumber <- as.factor(cust.cp.runs.df$PouchLotNumber)
+cust.cp.runs.df$PouchSerialNumber <- as.factor(cust.cp.runs.df$PouchSerialNumber)
 
 # calculate mean and standard deviation 
-cp.runs.control.avg <- aggregate(Cp ~ AssayName, cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], mean)
-summary.cp.runs.control <- summarySE(cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], measurevar="Cp", groupvars=c("AssayName"), conf.interval = 0.95, na.rm=TRUE)
+cust.cp.runs.control.avg <- aggregate(Cp ~ AssayName, cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], mean)
+summary.cust.cp.runs.control <- summarySE(cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], measurevar="Cp", groupvars=c("AssayName"), conf.interval = 0.95, na.rm=TRUE)
 
 # plot using standard deviation error bar
 dev.new()
 g3 <- ggplot() + ylab(cat("Cp", "\u00B1", "std.dev.")) + ggtitle("Customer control Cp data for [PouchLotNumber] 332916")
 g3 <- g3 + geom_point(data = cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], aes(x = AssayName, y = Cp), color = 'blue', size = 1, alpha = 0.25)
-g3 <- g3 + geom_point(data = cp.runs.control.avg, aes(x = AssayName, y = Cp), color = 'red', size = 2)
-g3 <- g3 + geom_errorbar(data = summary.cp.runs.control, aes(x = AssayName, ymin = Cp - sd, ymax = Cp + sd))
+g3 <- g3 + geom_point(data = cust.cp.runs.control.avg, aes(x = AssayName, y = Cp), color = 'red', size = 2)
+g3 <- g3 + geom_errorbar(data = summary.cust.cp.runs.control, aes(x = AssayName, ymin = Cp - sd, ymax = Cp + sd))
 g3
 
 # plot the Cp values for customer controls
@@ -121,7 +117,7 @@ gh3
 
 
 # plot internal vs. customer controls PCR1, PCR2, RNA
-internal_cp <- cp.pouch.lot.number.df.332916$Cp[cp.pouch.lot.number.df.332916$Name == "PCR2"]
+internal_cp <- internal.cp.pouch.lot.number.df.332916$Cp[internal.cp.pouch.lot.number.df.332916$Name == "PCR2"]
 customer_cp <- cust.cp.runs.df$Cp[cust.cp.runs.df$TargetName == 'PCR2 Control']
 customer_cp <- customer_cp[100:120]
 qplot(internal_cp, customer_cp)
@@ -129,13 +125,58 @@ qplot(internal_cp, customer_cp)
 
 
 
+# extract the internal Cp data for the lot numbers from customer Cp data -------------------------------------------------------------------------------------------
+for (i in 1:nrow(pouch.lot.number.df)){
+#for (i in 1:20){
+  temp <- get(paste0("internal.cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], sep = ""))
+  if(nrow(temp) > 0){
+    extracted.internal.cp.controls.df <- assign(paste0("internal.cp.pouch.lot.number.df.", pouch.lot.number.df$PouchLotNumber[i], sep = ""), temp)
+    extracted.internal.cp.controls.df$PouchSerialNumber <- as.factor(extracted.internal.cp.controls.df$PouchSerialNumber)
+    extracted.internal.cp.controls.df$PouchLotNumber <- as.factor(extracted.internal.cp.controls.df$PouchLotNumber)
+    extracted.internal.cp.controls.df$type <- 'internal'
+    extracted.internal.cp.controls.df <- subset(extracted.internal.cp.controls.df, select = -c(SampleType))
+    
+    names(extracted.internal.cp.controls.df)[names(extracted.internal.cp.controls.df) == 'Name'] <- 'AssayName'
+    customer.cp.controls.df <- cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control' & cust.cp.runs.df$PouchLotNumber == cust.cp.runs.df$PouchLotNumber[i], c('PouchSerialNumber', 'PouchLotNumber', 'AssayName', 'Cp')]
+    customer.cp.controls.df$type <- 'customer'
+    internal.and.customer.cp.controls <- rbind(extracted.internal.cp.controls.df, customer.cp.controls.df)
+  
+    g5 <- ggplot()
+    g5 <- g5 + geom_histogram(data = internal.and.customer.cp.controls, aes(Cp, fill = type), stat = "bin", bins = 10) 
+    g5 <- g5 + facet_wrap(type~AssayName )
+    # g5
+    # print(i)
+    # print(head(temp))
+    # 
+    # print plot
+    jpeg(filename = paste("Control_Cp_dist-", pouch.lot.number.df$PouchLotNumber[i], ".jpg", sep="")) #, width = 480, height = 480, units = 'px', res = 300)
+    print(g5) # Make plot
+    dev.off()
+    
+    # calculate mean and summarySE
+    internal.and.customer.cp.controls.avg <- aggregate(Cp ~ AssayName + type, internal.and.customer.cp.controls, mean)
+    summary.internal.and.customer.cp.controls <- summarySE(internal.and.customer.cp.controls, measurevar="Cp", groupvars=c("AssayName", "type"), conf.interval = 0.95, na.rm=TRUE)
+    
+    g6 <- ggplot() + ylab(paste("Cp", "\u00B1", "std.dev.")) + ggtitle(paste('Customer control Cp data for ',  pouch.lot.number.df$PouchLotNumber[i], sep = ""))
+    g6 <- g6 + geom_point(data = internal.and.customer.cp.controls, aes(x = AssayName, y = Cp), color = 'blue', size = 1, alpha = 0.25)
+    g6 <- g6 + facet_wrap(~ type)
+    g6 
+    
+    # g6 <- g6 + geom_point(data = cust.cp.runs.df[cust.cp.runs.df$ResultType == 'control', ], aes(x = AssayName, y = Cp), color = 'blue', size = 1, alpha = 0.25)
+    g6 <- g6 + geom_point(data = internal.and.customer.cp.controls.avg, aes(x = AssayName, y = Cp), color = 'red', size = 2)
+    g6 <- g6 + geom_errorbar(data = summary.internal.and.customer.cp.controls, aes(x = AssayName, ymin = Cp - sd, ymax = Cp + sd))
+    #g6
+    
+    # print plot
+    jpeg(filename = paste("Control_Cp_errorbar-", pouch.lot.number.df$PouchLotNumber[i], ".jpg", sep="")) #, width = 480, height = 480, units = 'px', res = 300)
+    print(g6) # Make plot
+    dev.off()
+    
+  }
+}
 
 
-
-
-
-
-
+# ------------------------------------------------------------------------------------------------------------------------
 
 
 
--- Nilesh/DataSources/SQL/20161129_NormalizedMetricforQuantificationonDisease/FADataWarehouse_query.sql
+++ Nilesh/DataSources/SQL/20161129_NormalizedMetricforQuantificationonDisease/FADataWarehouse_query.sql
@@ -11,6 +11,7 @@ SELECT DISTINCT
 	--R.[InstrumentSerialNumber]
 FROM [FADataWarehouse].[dbo].[RunData] R WITH(NOLOCK) INNER JOIN [FADataWarehouse].[dbo].[SummarizedPositiveAssayResults] S WITH(NOLOCK)
 		ON R.[RunDataId] = S.[RunDataId]
-WHERE R.[PouchTitle] LIKE 'Resp%' AND [CustomerSiteId] = 7
+WHERE R.[PouchTitle] LIKE 'Resp%' AND S.[CustomerSiteId] = 7 AND R.[PositiveAssays] >= 3
 ORDER BY R.[PouchLotNumber] --, R.[PouchSerialNumber], S.[Interpretation]
+
 
