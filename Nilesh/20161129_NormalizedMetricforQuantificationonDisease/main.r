609c75ad8a461926e0e5182b345667617106b02a
Bionilesh <nilesh.ingle@biofiredx.com>
2017-01-03 18:08:56 -0700

    20161129_NormalizedMetricforQuantificationonDisease
    - updated main.r to create plots with normalized Cp, mean normalized Cp,
    and centered rate (preliminary) for each TargetName
    - updated main.r to choose median Cp for AssayName for Influenza targets
    based on assay specificity
    - updated main.r to clean the code (more work to be done)

--- Nilesh/20161129_NormalizedMetricforQuantificationonDisease/main.r
+++ Nilesh/20161129_NormalizedMetricforQuantificationonDisease/main.r
@@ -28,7 +28,20 @@ query <- gsub("\\[CustomerSiteId\\] = [0-9]+", paste("[CustomerSiteId] = ", cust
 query <- gsub("AND S.\\[CustomerSiteId\\] = [0-9]+", "",query) 
 }
 pouch.lot.number.df <- sqlQuery(FADWcxn,query)
+
+queryVector <- scan(paste(sql_path,'/query_CenteredRate.txt', sep=""),what=character(),quote="")
+query <- paste(queryVector,collapse=" ")
+if (!is.na(customer_site_id)){
+  query <- gsub("\\[CustomerSiteId\\] = [0-9]+", paste("[CustomerSiteId] = ", customer_site_id, sep= ""), query)
+}else {
+  query <- gsub("AND S.\\[CustomerSiteId\\] = [0-9]+", "",query) 
+}
+centered.rate.df <- sqlQuery(FADWcxn,query)
+
 odbcClose(FADWcxn)
+
+# rename 'AssayName' to 'TargetName'
+names(centered.rate.df)[names(centered.rate.df) == 'AssayName'] <- 'TargetName'
 # ------------------------------------------------------------------------------------------------------------------------
 
 
@@ -89,16 +102,17 @@ all.customer.pouch.lot.number.df <- all.customer.pouch.lot.number.df[!is.na(all.
 # Code: Normalize Cp
 # ===============================================================================================================
 
-# take 'median' of Cp for each assay if >= 2 wells are positive
+# Calculate 'median' of Cp for each assay if >= 2 wells are positive .........
+# sort columns
 all.customer.pouch.lot.number.df <- all.customer.pouch.lot.number.df[order(all.customer.pouch.lot.number.df$RunDataId, all.customer.pouch.lot.number.df$TargetName, all.customer.pouch.lot.number.df$AssayName, all.customer.pouch.lot.number.df$WellDataId), ]
-# flag assay result
+# flag assay result for 'positive' assay as '1'
 all.customer.pouch.lot.number.df$FlagNUM_AssayPositive <- ifelse(all.customer.pouch.lot.number.df$AssayResult == 'Positive', 1, 0)
-#
+# subset non-control data and flag assay as 'Yes' if >=2 wells are positive
 temp.all.customer <- all.customer.pouch.lot.number.df[all.customer.pouch.lot.number.df$ResultType == 'organism', ]
 agg.temp.all.customer <- aggregate(FlagNUM_AssayPositive ~ RunDataId + AssayName, temp.all.customer, sum)
 names(agg.temp.all.customer)[names(agg.temp.all.customer) == 'FlagNUM_AssayPositive'] <- 'SUM_FlagNUM_AssayPositive'
 agg.temp.all.customer$FlagSTR_AssayPositive <- ifelse(agg.temp.all.customer$SUM_FlagNUM_AssayPositive >= 2, 'Yes', 'No')
-
+# merge the 'non-control' data back onto to the main table
 merge.agg.temp.all.customer <- merge(x = temp.all.customer, y = agg.temp.all.customer, by.x = c('RunDataId', 'AssayName'), by.y = c('RunDataId', 'AssayName'))
 new.all.customer.pouch.lot.number.df <- merge.agg.temp.all.customer[merge.agg.temp.all.customer$FlagSTR_AssayPositive == 'Yes', ]
 
@@ -112,6 +126,45 @@ new.all.customer.pouch.lot.number.df <- merge (x = all.customer.pouch.lot.number
 new.all.customer.pouch.lot.number.df$MedianCp <- ifelse(new.all.customer.pouch.lot.number.df$ResultType == 'control', NA, new.all.customer.pouch.lot.number.df$MedianCp)
 # order
 new.all.customer.pouch.lot.number.df <- new.all.customer.pouch.lot.number.df[order(new.all.customer.pouch.lot.number.df$RunDataId, new.all.customer.pouch.lot.number.df$TargetName, new.all.customer.pouch.lot.number.df$AssayName, new.all.customer.pouch.lot.number.df$WellDataId), ]
+
+
+
+# # implementing "do.call" to identify >= 2 (+)ve wells -----------
+# wells.df <- all.customer.pouch.lot.number.df
+# assay_name <- unique(wells.df$AssayName)
+# target_name <- unique(wells.df$TargetName)
+# run_data_id <- unique(wells.df$RunDataId)
+# temp <- do.call(rbind, lapply(1:length(run_data_id), function(x)
+#     do.call(rbind, lapply(1:length(target_name), function(y)
+#       do.call(rbind, lapply(1:length(assay_name), function(z)
+#           data.frame(
+#           RunDataId = wells.df$RunDataId[x],
+#           TargetName = wells.df$TargetName[y],
+#           AssayName = wells.df$AssayName[z],
+#           #Flag = ifelse(length(grep("Positive", unlist(strsplit(paste(wells.df$AssayResult[z], wells.df$AssayResult[z+1], wells.df$AssayResult[z+2], sep=" "), split=" ")))) >= 2, 'Yes', 'No')
+#           Flag = if_else(length(grep("Positive", wells.df$AssayResult[wells.df$RunDataId == run_data_id[x] & wells.df$TargetName == target_name[y] & wells.df$AssayName == assay_name[z]])) >= 2, "Yes", "No")
+#         )
+# ))))))
+# 
+#
+# # implementing "do.call" to calculate median ----------
+# cp.df <- all.customer.pouch.lot.number.df
+# #
+# cp.controls <- subset(cp.df, ResultType == 'control')
+# cp.df <- subset(cp.df, ResultType != 'control')
+# runs <- unique(cp.df$RunDataId)
+# cp.median <- do.call(rbind, lapply(1:length(runs), function(x)
+#   do.call(rbind, lapply(1:length(unique(cp.df[cp.df$RunDataId==runs[x], 'TargetName'])), function(y)
+#     do.call(rbind,  lapply(1:length(unique(cp.df[cp.df$RunDataId==runs[x] & cp.df$TargetName==unique(cp.df[cp.df$RunDataId==runs[x], 'TargetName'])[y],'AssayName'])), function(z)
+#       data.frame(RunDataId = runs[x],
+#                  TargetName = unique(cp.df[cp.df$RunDataId==runs[x], 'TargetName'])[y],
+#                  AssayName = unique(cp.df[cp.df$RunDataId==runs[x] & cp.df$TargetName==unique(cp.df[cp.df$RunDataId==runs[x], 'TargetName'])[y], 'AssayName'])[z],
+#                  MedianCp = median(cp.df[cp.df$RunDataId==runs[x] & cp.df$TargetName==unique(cp.df[cp.df$RunDataId==runs[x],'TargetName'])[y] & cp.df$AssayName==unique(cp.df[cp.df$RunDataId==runs[x] & cp.df$TargetName==unique(cp.df[cp.df$RunDataId==runs[x],'TargetName'])[y],'AssayName'])[z], 'Cp'])
+#       )
+#     ))
+#   ))
+# ))
+# # -----------------------------------------------------
 
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
@@ -159,9 +212,25 @@ dev.off()
 
 
 # for TARGET_NAME ----------------------------------------------------------------------------------------------------------------
+# in case of 'Influenza A%' target consider the 'MedianCp' of only the 'specific' assays for target
+# Influenza A H1: 'H1-pan' as positive
+# Influenza A H1-2009: 'H1-2009' as positive
+# Influenza A (no subtype detected): 'Apan1' and 'Apan2' as positive
+# Influenza A H3: 'H3' as positive
+temp_influ <- new.all.customer.pouch.lot.number.df
+temp_influ$MedianCp[temp_influ$TargetName == "Influenza A H1" & temp_influ$AssayName != "FluA-H1-pan" & temp_influ$ResultType != "control"] <- NA
+temp_influ$MedianCp[temp_influ$TargetName == "Influenza A H3" & temp_influ$AssayName != "FluA-H3" & temp_influ$ResultType != "control"] <- NA
+temp_influ$MedianCp[temp_influ$TargetName == 'Influenza A H1-2009' & temp_influ$AssayName != 'FluA-H1-2009' & temp_influ$ResultType != 'control'] <- NA
+temp_influ$MedianCp[temp_influ$TargetName == "Influenza A (no subtype detected)" & temp_influ$AssayName != "FluA-pan1" & temp_influ$AssayName != "FluA-pan2" & temp_influ$ResultType != "control"] <- NA
+
+
+
+
 # find MINIMUM of assays ---------------
 min.median.cp.for.target <- aggregate(MedianCp ~ RunDataId + TargetName, new.all.customer.pouch.lot.number.df, min)
 names(min.median.cp.for.target)[names(min.median.cp.for.target) == 'MedianCp'] <- 'MinMedianCp'
+
+
 # merge with the main table
 merge.min.median.cp.for.target <- merge(x = new.all.customer.pouch.lot.number.df, y = min.median.cp.for.target, by.x = c('RunDataId', 'TargetName'), by.y = c('RunDataId', 'TargetName'), all.x = TRUE )
 
@@ -177,26 +246,43 @@ merge_PCR1_min_median$NormalizedCp <- (merge_PCR1_min_median$Cp / merge_PCR1_min
 mean_target_normalizedCp <- aggregate(NormalizedCp ~ ConcatDate + TargetName, merge_PCR1_min_median, mean)
 names(mean_target_normalizedCp)[names(mean_target_normalizedCp) == 'NormalizedCp'] <- 'MeanNormalizedCp'
 
-# plot the mean of median cp for one assay for along ConcatDate (for an assay)
+# count 'RunDataId' per week
+count_rundataid_per_week <- count(unique(all.customer.pouch.lot.number.df[, c('ConcatDate', 'RunDataId')]), c('ConcatDate'))
+merge_PCR1_min_median <- merge(x = merge_PCR1_min_median, y = count_rundataid_per_week, by.x = 'ConcatDate', by.y = 'ConcatDate', all.x = TRUE)
+merge_PCR1_min_median_organism <- merge_PCR1_min_median[merge_PCR1_min_median$ResultType == 'organism', ]
+merge_PCR1_min_median_organism <- subset(merge_PCR1_min_median_organism, select = -c(freq.x))
+names(merge_PCR1_min_median_organism)[names(merge_PCR1_min_median_organism) == 'AssayName.x'] <- 'AssayName'
+names(merge_PCR1_min_median_organism)[names(merge_PCR1_min_median_organism) == 'freq.y'] <- 'freq'
+
+# summary for CI error bars
+summary_merge_PCR1_min_median_organism <- summarySE(merge_PCR1_min_median_organism, measurevar="NormalizedCp", groupvars=c("ConcatDate", "TargetName"), conf.interval = 0.95, na.rm=TRUE)
+
+# Create list of TargetName and AssayName for plotting
+assay_name <- unique(merge_PCR1_min_median_organism$AssayName[merge_PCR1_min_median_organism$ResultType == 'organism'])
+target_name <- unique(merge_PCR1_min_median_organism$TargetName[merge_PCR1_min_median_organism$ResultType == 'organism'])
+
 dateBreaks <- unique(as.character(mean_target_normalizedCp$ConcatDate))[order(unique(as.character(mean_target_normalizedCp$ConcatDate)))][seq(1, length(unique(as.character(mean_target_normalizedCp$ConcatDate))), 20)]
-g3 <- ggplot() + ggtitle("Normalized Target Cp") + scale_x_discrete(breaks=dateBreaks)
-g3 <- g3 + geom_point(data = mean_target_normalizedCp[grep(".*Rhino.*", mean_target_normalizedCp$TargetName),], aes(x = as.factor(ConcatDate), y = MeanNormalizedCp))
-g3
-
-# print plot
-jpeg(paste("Target_Cp", ".jpg", sep=""), width = 1500, height = 480, units = 'px')
-print(g3) # Make plot
-dev.off()
-
-
-
-
-
-#
-
-
-
-
+for (i in 1:length(target_name)){
+#for (i in 1:1){  
+  print(target_name[i])
+  #target <- target_name[i]
+  # plot the mean of median cp for one assay for along ConcatDate (for an assay)
+  g3 <- ggplot() +  xlab("Year-Week") + ylab(paste("Normalized Cp ", "\u00B1", "95% CI.")) + ggtitle(paste("Normalized Target Cp for ", target_name[i], sep="")) + scale_x_discrete(breaks=dateBreaks)
+  g3 <- g3 + geom_bar(data = centered.rate.df[grep(target_name[i], centered.rate.df$TargetName), ], aes(x = as.factor(ConcatDate), y = CenteredRate*10), stat = "identity", alpha = 0.5) 
+  g3 <- g3 + geom_point(data = merge_PCR1_min_median_organism[grep(target_name[i], merge_PCR1_min_median_organism$TargetName), ], aes(x = as.factor(ConcatDate), y = NormalizedCp, color = freq),  alpha = 0.1, size = 3) 
+  g3 <- g3 + geom_errorbar(data = summary_merge_PCR1_min_median_organism[grep(target_name[i], summary_merge_PCR1_min_median_organism$TargetName), ], aes(x = as.factor(ConcatDate), ymin = NormalizedCp - ci, ymax = NormalizedCp + ci))
+  #g3 <- g3 + geom_boxplot(data = merge_PCR1_min_median[grep(".*Rhino.*", merge_PCR1_min_median$TargetName), ], aes(x = as.factor(ConcatDate), y = NormalizedCp), stat = "boxplot")
+  g3 <- g3 + geom_point(data = mean_target_normalizedCp[grep(target_name[i], mean_target_normalizedCp$TargetName), ], aes(x = as.factor(ConcatDate), y = MeanNormalizedCp), color = 'red')
+  g3
+  
+  #target <- ifelse(grepl("/", target_name[i]), gsub("/", " ",target_name[i]), target_name[i])
+  target <- if(grepl("/", target_name[i])){gsub("/", " ",target_name[i])} else {target_name[i]}
+  print(target)
+  # print plot
+  jpeg(filename = paste("Target_Cp--", target, ".jpg", sep=""), width = 1500, height = 480, units = 'px')
+  print(g3) # Make plot
+  dev.off()
+}
 
 
 
